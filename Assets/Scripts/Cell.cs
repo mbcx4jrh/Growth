e76ea30de5d4ea63d5d001a6988c15e648de97cf
Joe <joe.heath@gmail.com>
2021-11-02 18:27:23 +0000

    got spring force working

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -6,12 +6,19 @@ namespace growth {
     public class Cell {
 
         public Vector3 position;
+        public Vector3 normal;
+
         public List<Cell> neighbours = new List<Cell>(5);
 
         public CellGameObject gameObject;
         
-        public Cell(Vector3 position) {
+        public Cell(Vector3 position, Vector3 normal) {
             this.position = position;
+            this.normal = normal;
+        }
+
+        public static Cell CellOnSphere(Vector3 position) {
+            return new Cell(position, position.normalized);
         }
     }
 }
--- Assets/Scripts/CellularForm.cs
+++ Assets/Scripts/CellularForm.cs
@@ -8,11 +8,60 @@ namespace growth {
 
         public CellGameObject cellPrefab;
 
+        [Range(0.1f,2f)]
+        public float linkLength = 1f;
+
+        [Range(0f, 1f)]
+        public float springFactor;
+
+        [Range(0f, 1f)]
+        public float planarFactor = 1f;
+
+        [Range(0f, 1f)]
+        public float bulgeFactor = 1f;
+
         Cell[] cells;
 
         private void Start() {
             GenerateInitialCells();
             GenerateGameObjectsForCells();
+        }
+
+        private void Update() {
+            CalculateForces();
+            UpdateCells();
+        }
+
+        private void UpdateCells() {
+            foreach (var cell in cells) {
+                cell.gameObject.transform.position = cell.position;
+            }
+        }
+
+        private void CalculateForces() {
+            var link2 = linkLength * linkLength;
+            foreach (var cell in cells) {
+                var d_spring_sum = Vector3.zero;
+                var d_planar_sum = Vector3.zero;
+                var d_bulge_sum = 0f;
+                int n = cell.neighbours.Count;
+                foreach (var neighbour in cell.neighbours) {
+                    var vectorBetween = neighbour.position - cell.position;
+                    var distance2 = vectorBetween.sqrMagnitude;
+                    var distance = vectorBetween.magnitude;
+                    var t = neighbour.position + cell.normal*linkLength;
+                    float theta = Vector3.Angle(t - cell.position, vectorBetween)*Mathf.Deg2Rad;
+                    d_spring_sum += -cell.position +neighbour.position - vectorBetween.normalized*linkLength;
+                    d_planar_sum += neighbour.position;
+                    d_bulge_sum += Mathf.Sqrt(Mathf.Max(link2 + distance2 - 2 * linkLength * distance * Mathf.Cos(theta)));
+                }
+                var d_spring = linkLength*d_spring_sum / n;
+                var d_planar = d_planar_sum / n;
+                var d_bulge = cell.normal * (d_bulge_sum / n);
+
+                //apply forces
+                cell.position += Time.deltaTime * (springFactor * d_spring + planarFactor * d_planar + bulgeFactor * d_bulge);
+            }
         }
 
         private void GenerateGameObjectsForCells() {
@@ -33,7 +82,7 @@ namespace growth {
         private void GenerateInitialCells() {
             cells = new Cell[12];
             for (int i = 0; i < Icosphere.vertices.Length; i++) {
-                cells[i] = new Cell(Icosphere.vertices[i]);
+                cells[i] = Cell.CellOnSphere(Icosphere.vertices[i]);
             }
             for (int i = 0; i < 12; i++) {
                 for (int j = 0; j < 5; j++) {
