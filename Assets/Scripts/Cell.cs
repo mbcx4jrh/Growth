7d62995921373ddd3fd359e06d9ceeb51fa80980
Joe <joe.heath@gmail.com>
2021-11-02 21:46:48 +0000

    added repulsive force

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace growth {
@@ -12,7 +13,7 @@ namespace growth {
         public List<Cell> neighbours = new List<Cell>(5);
 
         public CellGameObject gameObject;
-        
+
         public Cell(Vector3 position, Vector3 normal) {
             this.position = position;
             this.normal = normal;
@@ -21,5 +22,53 @@ namespace growth {
         public static Cell CellOnSphere(Vector3 position) {
             return new Cell(position, position.normalized);
         }
+
+        public void ReplaceLink(Cell old, Cell neo) {
+            int i = neighbours.IndexOf(old);
+            neighbours[i] = neo;
+        }
+
+        public Cell Split() {
+            var daughter = new Cell(position, normal);
+            if (neighbours.Count == 0) return daughter;
+
+            //find nearest neighbour
+            float nearest_d = float.MaxValue;
+            int nearest = -1;
+            for (int i = 0; i < neighbours.Count; i++) {
+                float d = (neighbours[i].position - position).sqrMagnitude;
+                if (d < nearest_d) {
+                    nearest_d = d;
+                    nearest = i;
+                }
+            }
+
+            var newLinks = new List<Cell>();
+            var splitPlane = new Plane(position, neighbours[nearest].position, position + normal);
+            daughter.neighbours.Add(this);
+            daughter.neighbours.Add(neighbours[nearest]);
+            neighbours[nearest].neighbours.Add(daughter);
+            newLinks.Add(daughter);
+            newLinks.Add(neighbours[nearest]);
+
+            for (int i=0; i<neighbours.Count; i++) {
+
+                if (i == nearest) continue;
+                var n = neighbours[i];
+             
+                if (splitPlane.GetSide(n.position)) {
+                    daughter.neighbours.Add(n);
+                    n.ReplaceLink(this, daughter);
+                }
+                else {
+                    newLinks.Add(n);
+                }
+            }
+            neighbours = newLinks;
+            food = 0f;
+            return daughter;
+        }
+
+
     }
 }
--- Assets/Scripts/CellularForm.cs
+++ Assets/Scripts/CellularForm.cs
@@ -6,18 +6,25 @@ using UnityEngine;
 namespace growth {
     public class CellularForm : MonoBehaviour {
 
+        public int maxCells = 1000;
 
-        [Range(0.1f,2f)]
+        [Range(0.1f, 2f)]
         public float linkLength = 1f;
 
-        [Range(0f, 1f)]
-        public float springFactor;
+        [Range(0f, 5f)]
+        public float springFactor = 1f;
 
         [Range(0f, 1f)]
         public float planarFactor = 1f;
 
         [Range(0f, 1f)]
         public float bulgeFactor = 1f;
+
+        [Range(0f, 10f)]
+        public float repulsionRange = 2f;
+
+        [Range(0f, 1f)]
+        public float repulsionFactor = 1f;
 
         public CellGameObject cellPrefab;
 
@@ -27,7 +34,7 @@ namespace growth {
         public FoodSource[] foodSources;
 
         [HideInInspector]
-        public Cell[] cells;
+        public List<Cell> cells;
 
         private void Start() {
             GenerateInitialCells();
@@ -41,12 +48,27 @@ namespace growth {
 
         private void Update() {
             FeedCells();
+            CheckForSplits();
             CalculateForces();
             UpdateCells();
         }
 
+        private void CheckForSplits() {
+            int n = cells.Count;
+            if (n >= maxCells) return;
+            for (int i = 0; i < n; i++) {
+                if (cells[i].food >= 1f) {
+                    var newCell = cells[i].Split();
+                    cells.Add(newCell);
+                    GenerateGameObjectsForCell(newCell);
+                    if (cells.Count == maxCells) Debug.Log("Maximum cells reached (" + maxCells + ")");
+                }
+            }
+        }
+
+
         private void FeedCells() {
-            foreach(var foodSource in foodSources) {
+            foreach (var foodSource in foodSources) {
                 foodSource.Feed(this);
             }
         }
@@ -59,27 +81,43 @@ namespace growth {
 
         private void CalculateForces() {
             var link2 = linkLength * linkLength;
+            var r2 = repulsionRange * repulsionRange;
             foreach (var cell in cells) {
                 var d_spring_sum = Vector3.zero;
                 var d_planar_sum = Vector3.zero;
                 var d_bulge_sum = 0f;
+                var d_collision_sum = Vector3.zero;
                 int n = cell.neighbours.Count;
                 foreach (var neighbour in cell.neighbours) {
                     var vectorBetween = neighbour.position - cell.position;
                     var distance2 = vectorBetween.sqrMagnitude;
                     var distance = vectorBetween.magnitude;
-                    var t = neighbour.position + cell.normal*linkLength;
-                    float theta = Vector3.Angle(t - cell.position, vectorBetween)*Mathf.Deg2Rad;
-                    d_spring_sum += -cell.position +neighbour.position - vectorBetween.normalized*linkLength;
+                    var t = neighbour.position + cell.normal * linkLength;
+                    float theta = Vector3.Angle(t - cell.position, vectorBetween) * Mathf.Deg2Rad;
+                    d_spring_sum += -cell.position + neighbour.position - vectorBetween.normalized * linkLength;
                     d_planar_sum += vectorBetween;
                     d_bulge_sum += Mathf.Sqrt(Mathf.Max(link2 + distance2 - 2 * linkLength * distance * Mathf.Cos(theta)));
+                    d_collision_sum += vectorBetween.normalized * ((r2 - distance2) / r2);
                 }
-                var d_spring = linkLength*d_spring_sum / n;
+                var d_spring = linkLength * d_spring_sum / n;
                 var d_planar = d_planar_sum / n;
                 var d_bulge = cell.normal * (d_bulge_sum / n);
 
+                int nearby = cell.neighbours.Count;
+                foreach (var other in cells) {
+                    var between = cell.position - other.position;
+                    if (between.sqrMagnitude < r2) {
+                        d_collision_sum += between * ((r2 - between.sqrMagnitude) / r2);
+                        nearby += 1;
+                    }
+                }
+                var d_collision = d_collision_sum / nearby;
                 //apply forces
-                cell.position += Time.deltaTime * (springFactor * d_spring + planarFactor * d_planar + bulgeFactor * d_bulge);
+                cell.position += Time.deltaTime * (springFactor * d_spring
+                                                 + planarFactor * d_planar
+                                                 + bulgeFactor * d_bulge
+                                                 + repulsionFactor * d_collision);
+
             }
         }
 
@@ -96,7 +134,7 @@ namespace growth {
         private void GenerateGameObjectsForCell(Cell cell) {
             cell.gameObject = Instantiate(cellPrefab, cell.position, Quaternion.identity);
             cell.gameObject.cell = cell;
-            if (cellsParent !=null) {
+            if (cellsParent != null) {
                 cell.gameObject.transform.parent = cellsParent.transform;
             }
             else {
@@ -105,9 +143,9 @@ namespace growth {
         }
 
         private void GenerateInitialCells() {
-            cells = new Cell[12];
+            cells = new List<Cell>();
             for (int i = 0; i < Icosphere.vertices.Length; i++) {
-                cells[i] = Cell.CellOnSphere(Icosphere.vertices[i]);
+                cells.Add(Cell.CellOnSphere(Icosphere.vertices[i]));
             }
             for (int i = 0; i < 12; i++) {
                 for (int j = 0; j < 5; j++) {
