cde223bece0db6b45036c6be43fc2c68859a4ec2
Joe <joe.heath@gmail.com>
2021-11-06 12:08:44 +0000

    started building interface

--- Assets/Scripts/Interface/MainInterface.cs
+++ Assets/Scripts/Interface/MainInterface.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace growth {
     public class MainInterface : MonoBehaviour {
 
-        public CellularForm cellularForm;
-
-        
         
 
         public void Quit() {
             Application.Quit();
+        }
+
+        public void InvestigateForm() {
+            SceneManager.LoadScene("InvestigateForm");
         }
     }
 }
